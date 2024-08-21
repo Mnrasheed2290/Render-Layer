b84c3305e5ff40aeafc3c04874566883b26b926d
Mnrasheed2290 <naim.maria2@gmail.com>
2024-08-21 15:55:48 -0500

    debugging

--- approach/test_open.cv.cpp
+++ approach/test_open.cv.cpp
@@ -2,18 +2,11 @@
 #include <iostream>
 
 int main() {
- std::cout << "Testing OpenCV Installation" << std::endl;
-
- // Create a simple black image
- cv::Mat image = cv::Mat::zeros(300, 300, CV_8UC3);
-
- // Draw a white diagonal line
- cv::line(image, cv::Point(0, 0), cv::Point(300, 300), cv::Scalar(255, 255, 255), 5);
-
- // Display the image
- cv::imshow("Test Image", image);
- cv::waitKey(0);
-
- std::cout << "OpenCV is working correctly!" << std::endl;
- return 0;
+    cv::Mat image = cv::Mat::zeros(100, 100, CV_8UC3);
+    if (image.empty()) {
+        std::cout << "Failed to create OpenCV Mat!" << std::endl;
+        return -1;
+    }
+    std::cout << "OpenCV is working!" << std::endl;
+    return 0;
 }
